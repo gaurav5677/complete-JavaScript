b748eefd92601a166b67fe22128812c9925b5047
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-01 11:35:37 +0530

    values and variables

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,9 @@
-let js = 'amazing';
-if (js == 'amazing') alert('Javascript is fun!');
+let js ="amazing";
+console.log(40+8+23-10);
+console.log("jonas"); 
+console.log(23);
 
-console.log(40 + 8 + 23 - 10);
+// a value is the smallest unit  of  information that we have in javascript
+ 
+let firstName="Gaurav";    // this is how we declare a variable
+console.log(firstName);
