4cefd2f967bd894f8a2ef7bebe0b7c30f99c4ce8
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-01 14:56:23 +0530

    data types

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js ="amazing";
 console.log(40+8+23-10);
 console.log("jonas"); 
@@ -7,3 +8,34 @@ console.log(23);
  
 let firstName="Gaurav";    // this is how we declare a variable
 console.log(firstName);
+
+// _ and $ are the onlu two symbols that are allowed for naming variables
+
+//starting letter with uppercase is only use while oop(object oriented programming) \
+
+let myFirstjob = 'Programmer';
+let mycurrentjob = 'Teacher';
+console.log(mycurrentjob)
+console.log(myFirstjob)
+*/
+
+let javascriptIsFun=true; // value hold the datatype not variable 
+console.log(javascriptIsFun)
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'gaurav');
+    
+javascriptIsFun = 'Yes!'// when you want to change the value simpliy rewrite it without writing let 
+console.log(typeof javascriptIsFun)
+
+let year;
+console.log(year);
+
+console.log(typeof year);
+
+year = 1992;
+console.log(typeof year)
+
+console.log(typeof null);  
