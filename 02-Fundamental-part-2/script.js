1cc3d41a171be46ec6340b3fdbc12343bde6e4be
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-02 15:43:47 +0530

    Functions

---
+++ 02-Fundamental-part-2/script.js
@@ -0,0 +1,27 @@
+///////////////////////////Function/////////////////////////////
+
+/*
+function logger() {
+  console.log(`my name is gaurav`);
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 2);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 2));
+
+const appleOrangeJuice = fruitProcessor(2, 6);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
