055292418de3bd1bbd8d8e827565c025ba08afc5
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-02 12:43:52 +0530

    Switch Statements🥱

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -285,3 +285,30 @@ if (shouldDrive && !isTired) {
 
 
 */
+/*
+////////////////////Switch state //////////////////
+
+const day = "monday";
+
+switch (day) {
+  case "monday": // day === monday
+    console.log("plan course structure");
+    console.log("go to coding meetup");
+    break;
+  case "tuesday":
+    console.log(`Prepare theory videos`);
+    break;
+  case "wed":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log(`record videos`);
+  case `saturday`:
+  case "sunday":
+    console.log("enjoy the weekend :D");
+    break;
+  default:
+    console.log(`not a valid day!`);
+}
+*/
