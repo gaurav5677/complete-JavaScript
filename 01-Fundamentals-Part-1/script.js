d622267c97bd65856d5cd53a9e0c4d2316dbc13f
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-02 11:27:06 +0530

    Truthy and false value

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -198,4 +198,24 @@ let x = "10" - "4" - "3" - 2 + "5 ";
 console.log(x);
 */
 
-/// 5 falsy values : 0 , '' , undefined , null , NaN , falses
+/// 5 falsy values : 0 , '' , undefined , null ,    NaN , falses
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 10;
+if (money) {
+  console.log(`don't spent it all  `);
+} else {
+  console.log(`Your should get a job`);
+}
+
+let height;
+if (height) {
+  console.log(`Yey! height is defined`);
+} else {
+  console.log(`Height is undefiend `);
+}
