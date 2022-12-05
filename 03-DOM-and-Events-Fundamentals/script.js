10d5e217d5cd7ac29d66a303e4d8501a28bc47de
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-05 17:23:01 +0530

    🙂

--- 03-DOM-and-Events-Fundamentals/script.js
+++ 03-DOM-and-Events-Fundamentals/script.js
@@ -3,3 +3,6 @@ document.querySelector(".message").textContent = " Correct Number 🎉";
 
 document.querySelector(".number").textContent = 13;
 document.querySelector(".score").textContent = 11;
+
+document.querySelector(".guess").value = 23;
+console.log(document.querySelector(".guess").value);
