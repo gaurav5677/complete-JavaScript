1b6f2babf2977022a3af175752790020f54a6220
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-05 17:15:12 +0530

    starting😵 Selecting and Manipulating Element

--- 03-DOM-and-Events-Fundamentals/script.js
+++ 03-DOM-and-Events-Fundamentals/script.js
@@ -1,1 +1,5 @@
 console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = " Correct Number 🎉";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 11;
