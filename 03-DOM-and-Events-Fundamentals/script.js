07df2549adfb9e5d15b1bd9da9eccc51dd5a6dc5
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-06 15:46:20 +0530

    implemented the again aka reset feature

--- 03-DOM-and-Events-Fundamentals/script.js
+++ 03-DOM-and-Events-Fundamentals/script.js
@@ -10,7 +10,7 @@ console.log(document.querySelector(".guess").value);
 */
 
 ////////////////////////////////////////////// Secret Number ///////////////////////////////////////////////////////////////////
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 
 document.querySelector(".number").textContent = secretNumber;
 
@@ -18,7 +18,7 @@ document.querySelector(".number").textContent = secretNumber;
 //////////////////////////////////////////// Score data  ///////////////////////////////////////////////////////////////////
 
 let score = 20;
-////////////////////////////////////////////    Handling click event ////////////////////////////////////////////////////
+////////////////////////////////////////////    Handling check event ////////////////////////////////////////////////////
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -58,4 +58,17 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   }
+});
+
+////////////////////////////////////////////    Handling Again  event ////////////////////////////////////////////////////
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = "20";
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector(".message").textContent = "Start guessing.....";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
 });
