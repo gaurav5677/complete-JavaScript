cfa867012a633ecae332db2fb9a5d02342d61c2f
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-09 15:42:14 +0530

    Scoping

---
+++ 03-DOM-and-Events-Fundamentals/04-Behind-the-Scenes/script.js
@@ -0,0 +1,15 @@
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+
+  function printAge() {
+    const output = `you are ${age}, born in ${birthYear}`;
+    console.log(output);
+  }
+
+  printAge();
+  return age;
+}
+
+const firstName = "gaurav";
+calcAge(2001);
