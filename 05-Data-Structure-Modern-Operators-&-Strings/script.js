72bca11f5ae56b5c9f31ac48831c206e86ba0fb3
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-11 13:57:23 +0530

    Destructuring Object

--- 05-Data-Structure-Modern-Operators-&-Strings/script.js
+++ 05-Data-Structure-Modern-Operators-&-Strings/script.js
@@ -55,3 +55,77 @@ console.log(p, q, r);
 
 
 */
+
+////////////////////////////////// Destructruing Objects ////////////////////////////
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thus: {
+      open: 12,
+      close: 22,
+    },
+
+    fri: {
+      open: 11,
+      close: 20,
+    },
+
+    sat: {
+      open: 8,
+      close: 16,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `order received! ${this.starterMenu[starterIndex]}  and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: "22:20",
+  address: "via del sale, 23",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter);
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 12 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
