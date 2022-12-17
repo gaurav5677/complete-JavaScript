1e143d8b9e7c5c279a6d3dd4c5ddc3ec90c93e8a
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2022-12-17 22:55:58 +0530

    Values vs reference

--- 06-A-Closer-Look-At-Functions/script.js
+++ 06-A-Closer-Look-At-Functions/script.js
@@ -1,4 +1,4 @@
-const bookings = [];
+/*const bookings = [];
 const creatBooking = function (flightNum, numPassengers = 1, price = 199) {
   //        ES5
   //    numPassengers = numPassengers || 1;
@@ -17,3 +17,30 @@ creatBooking("LH123");
 creatBooking("LH124", 2, 800);
 creatBooking("LH124", 34);
 creatBooking("LH124", 4);
+*/
+///////////////// How Passing Arguments Works Value VS Reference/////////////////
+
+const flight = "LH234";
+const gaurav = {
+  name: "Gaurav Patil",
+  passport: 234234234,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LG242";
+  passenger.name = "Mr." + passenger.name;
+
+  if (passenger.passport === 234234234) {
+    alert("Checked In");
+  } else {
+    alert("Wrong passport");
+  }
+};
+
+checkIn(flight, gaurav);
+console.log(flight);
+console.log(gaurav);
+
+// is the same as doning
+const flightNum = flight;
+const passenger = gaurav;
