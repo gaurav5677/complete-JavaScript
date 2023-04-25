b1bb6f65f7cbbcf429908548ddaa45195710e2e1
gaurav5677 <85819394+gaurav5677@users.noreply.github.com>
2023-04-25 17:14:39 +0530

    Prototype🫠

--- 09-OOPs-in-JavaScript/main.js
+++ 09-OOPs-in-JavaScript/main.js
@@ -1,6 +1,6 @@
 'use strict';
 // The purpose of "use strict" is to indicate that the code should be executed in "strict mode"
-const person = function (firstName, birthYear) {
+const Person = function (firstName, birthYear) {
    //  console.log(this)
 
    // this are Instance properties 
@@ -17,7 +17,7 @@ const person = function (firstName, birthYear) {
 
 }
 
-const gaurav = new person('gaurav ', 2001);
+const gaurav = new Person('gaurav ', 2001);
 
 console.log(gaurav);
 // 1 . new Empty object is created {};
@@ -25,14 +25,14 @@ console.log(gaurav);
 // 3 . this new created object { } is linked to the prototype 
 // 4 . function automatically returns the empty {} 
 
-const abhishek = new person('abhi', 2001);
+const abhishek = new Person('abhi', 2001);
 
-const devesh = new person('devesh ', 2000);
+const devesh = new Person('devesh ', 2000);
 
 console.log(abhishek, devesh);
 // console.log(devesh);
 
-console.log(gaurav instanceof person);
+console.log(gaurav instanceof Person);
 // gaurav abhisshek and devesh are the instance of construction function Person
 
 const Admi = function (Naam, Gaddi) {
@@ -44,3 +44,23 @@ const Admi = function (Naam, Gaddi) {
 const gaurav1 = new Admi(' gaurav ', 'honda');
 
 console.log(gaurav1);
+
+
+
+////////////////////////////////// Prototypes  /////////////////////
+
+// each and every function in javaScript have a property called Prototype 
+
+Person.prototype.calcAge = function () {
+   console.log(2023 - this.birthYear);
+}
+
+devesh.calcAge();
+
+console.log(gaurav.__proto__);
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(gaurav.species, abhishek.species);
+
+console.log(gaurav.hasOwnProperty('firstName'));
+console.log(gaurav.hasOwnProperty('species'));
